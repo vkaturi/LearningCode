25f2f85d16e3035a86b875466605b1b7787a6095
venkat <venkat@VIT.COM>
2019-01-18 10:27:31 +0530

    Architecture, mysql and Design Patterns docs added

---
+++ PHP/Design Patterns/AdapterPattern.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+
+The Adapter design pattern as the name suggests is like real life adapter that converts one thing into some other one. Similarly, it can help us wrap functionality of some object into adapter providing us with consistent methods/API to work with. Understanding through an example is always the best way to learn.
+
+*/
+
+
+?>
