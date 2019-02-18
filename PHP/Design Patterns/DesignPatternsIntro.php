71b328a374dc15daa23797f4f8ad73b7bb74e1ec
venkat <venkat@VIT.COM>
2019-02-18 17:45:03 +0530

    AWS Content Added

---
+++ PHP/Design Patterns/DesignPatternsIntro.php
@@ -0,0 +1,15 @@
+<?php
+
+Design patterns are solutions for commonly recurring problems in object oriented design.
+
+Why study design patterns?
+--------------------------
+Avoid reinventing the solution.
+
+We can benefit from the experience of other designers who have successfully applied these patterns in their designs. We get ready-made high quality solutions to commonly recurring problems in design.
+
+Applying design patterns in your project will make it reusable, flexible and easily maintainable. There is no need of redesigning every time, the design has to be reused.
+
+
+
+?>
